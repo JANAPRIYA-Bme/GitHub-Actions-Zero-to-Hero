d7bab9f8d16db630e4de7ff42ea52056ce6ef3eb
JANAPRIYA-Bme <muruganagp8697@gmail.com>
2026-09-07 08:52:07 +0000

    Add addition module with tests

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,6 @@
-# app.py
-#first commit
-# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
